5d5b1f7dc0ccda4a261a9e796ccfd7555c497fe8
Weihang Lo <weihanglotw@gmail.com>
2017-10-21 10:39:56 +0800

    [Binary Tree] binary-tree-preorder-traversal

---
+++ 144.binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Problem: https://leetcode.com/problems/binary-tree-preorder-traversal/
+#
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+# 1. Recursive
+class Solution1(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        arr = []
+        self.preorder(root, arr)
+        return arr
+
+    def preorder(self, node, arr):
+        if node:
+            arr.append(node.val)
+            self.preorder(node.left, arr)
+            self.preorder(node.right, arr)
+
+
+# 2. Iterative
+class Solution2(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        arr = []
+        stack = []
+        node = root
+        while stack or node:
+            if node:
+                stack.append(node)
+                arr.append(node.val)  # Append before proceed to left node
+                node = node.left
+            else:
+                n = stack.pop()
+                node = n.right
+        return arr
+
+
+# 3. Another iterative solution. (Only append right node to stack)
+class Solution3(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        arr = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            while node:
+                arr.append(node.val)
+                if node.right:
+                    stack.append(node.right)
+                node = node.left
+        return arr
--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # LeetCode Solutions
 
+## Binary Tree
+
 - [x] 094. Binary Tree Inorder Traversal
-- [x] 102. Binary Tree-level Order Traversal
+- [x] 102. Binary Tree Level Order Traversal
 - [x] 104. Maximum Depth of Binary Tree
-- [x] 107. Binary Tree Level Order Traversal II 
+- [x] 107. Binary Tree Level Order Traversal II
 - [x] 110. Balanced Binary Tree
 - [x] 111. Minimum Depth of Binary TreekA
+- [ ] 112. Path Sum
+- [ ] 114. Flatten Binary Tree to Linked List
+- [x] 144. Binary Tree Preorder Traversal
+- [ ] 145. Binary Tree Postorder Traversal
 - [x] 226. Invert Binary Tree
-- [x] 654. Maximum Binary Tree 
+- [ ] 257. Binary Tree Paths
+- [ ] 310. Minimum Height Trees
+- [ ] 637. Average of Levels in Binary Tree
+- [x] 654. Maximum Binary Tree
+- [ ] 662. Maximum Width of Binary Tree
+- [ ] 687. Longest Univalue Path
 
-_Weihang Lo_
-
+🙃 _Weihang Lo_
